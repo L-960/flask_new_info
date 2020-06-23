e3c0f4a54a40357a6eb41a57b150ab91c48ebd2d
L-960 <60659961+L-960@users.noreply.github.com>
2020-06-23 19:53:46 +0800

    Update views.py

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -6,4 +6,21 @@ from info.modules.news import news_blu
 
 @news_blu.route('/<int:news_id>')
 def news_detail(news_id):
-    return render_template('news/detail.html',data=None)
+
+    # 在session中获取当前登陆用户的信息
+    user_id = session.get('user_id')
+
+    user = None
+
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+
+    data = {
+        # 如果存在就返回，不存在就返回空
+        "user_info": user.to_dict() if user else None,
+    }
+
+    return render_template('news/detail.html', data=data)
